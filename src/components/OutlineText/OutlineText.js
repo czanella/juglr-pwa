b30beae98f2ef017233766d44d9ffb497084c13e
Carlos Zanella <czanella@hotmail.com>
2018-07-22 22:01:49 -0300

    Adding OutlineText (WIP - needs bugfix)

---
+++ src/components/OutlineText/OutlineText.js
@@ -0,0 +1,76 @@
+import React, { Component, createRef } from 'react';
+import { number, string } from 'prop-types';
+
+import styles from './styles.scss';
+
+const propTypes = {
+    size: number,
+    strokeWidth: number,
+    children: string.isRequired,
+    className: string,
+};
+
+const defaultProps = {
+    size: 20,
+    strokeWidth: 5,
+    className: null,
+};
+
+class OutlineText extends Component {
+    constructor(props) {
+        super(props);
+
+        this.canvas = createRef();
+    }
+
+    componentDidMount() {
+        this.context = this.canvas.current.getContext('2d');
+        this.renderText();
+    }
+
+    componentDidUpdate() {
+        this.renderText();
+    }
+
+    renderText() {
+        const { children, size, strokeWidth } = this.props;
+        
+        // Resize the canvas
+        this.context.font = `${size}pt Pixellari`;
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        const canvasHeight = size + 8 * strokeWidth;
+        const canvasWidth = this.context.measureText(children).width + 4 * strokeWidth;
+        this.canvas.current.width = canvasWidth;
+        this.canvas.current.height = canvasHeight;
+
+        // Resets the text options
+        this.context = this.canvas.current.getContext('2d');
+        this.context.font = `${size}pt Pixellari`;
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+
+        // Draws the text
+        this.context.lineWidth = 4 * strokeWidth;
+        this.context.strokeStyle = styles.red;
+        this.context.strokeText(children, canvasWidth / 2, canvasHeight / 2);
+
+        this.context.lineWidth = 2 * strokeWidth;
+        this.context.strokeStyle = styles.black;
+        this.context.strokeText(children, canvasWidth / 2, canvasHeight / 2);
+
+        this.context.fillStyle = styles.white;
+        this.context.fillText(children, canvasWidth / 2, canvasHeight / 2);
+    }
+
+    render() {
+        const { className } = this.props;
+
+        return <canvas className={className} ref={this.canvas} />;
+    }
+}
+
+OutlineText.propTypes = propTypes;
+OutlineText.defaultProps = defaultProps;
+
+export default OutlineText;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import OutlineText from '../../components/OutlineText';
 import logo from './logo.png';
 
 import styles from './styles.scss';
 
-function Home() {
-    return (
-        <div className={styles.home}>
-            <img src={logo} className={styles.logo} alt={'Juglr'} />
-        </div>
-    )
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0,
+        };
+    }
+
+    componentDidMount() {
+        this.intervalId = window.setInterval(this.increaseCount, 1000);
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.intervalId);
+    }
+
+    increaseCount = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    render() {
+        const { count } = this.state;
+        return (
+            <div className={styles.home}>
+                <img src={logo} className={styles.logo} alt={'Juglr'} />
+                <OutlineText size={40} strokeWidth={5}>
+                    {count.toString()}
+                </OutlineText>
+            </div>
+        );
+    }
 }
 
 export default Home;
