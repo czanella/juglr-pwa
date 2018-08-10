07dc44c39036793012c3f5b8825a5ad27384dc8a
Carlos Zanella <czanella@hotmail.com>
2018-08-10 01:09:59 -0300

    Home layout

--- src/App/App.js
+++ src/App/App.js
@@ -13,7 +13,7 @@ function App() {
             <Helmet>
                 <title>Juglr</title>
             </Helmet>
-            <Route exact path={'/'} component={Home} />
+            <Home />
         </div>
     );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,38 +1,25 @@
 import React, { Component } from 'react';
-import OutlineText from '../../components/OutlineText';
 import logo from './logo.png';
+import playButton from './playButton.png';
+import soundButton from './soundButton.png';
+import aboutButton from './aboutButton.png';
 
 import styles from './styles.scss';
 
 class Home extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            count: 0,
-        };
-    }
-
-    componentDidMount() {
-        this.intervalId = window.setInterval(this.increaseCount, 1000);
-    }
-
-    componentWillUnmount() {
-        window.clearInterval(this.intervalId);
-    }
-
-    increaseCount = () => {
-        this.setState({ count: this.state.count + 1 });
     }
 
     render() {
-        const { count } = this.state;
         return (
             <div className={styles.home}>
                 <img src={logo} className={styles.logo} alt={'Juglr'} />
-                <OutlineText size={40} strokeWidth={4}>
-                    {count.toString()}
-                </OutlineText>
+                <div className={styles.buttons}>
+                    <img src={playButton} className={styles.play} alt={'Play'} />
+                    <img src={soundButton} className={styles.sound} alt={'Sound'} />
+                    <img src={aboutButton} className={styles.about} alt={'About'} />
+                </div>
             </div>
         );
     }
